3a52d5b9c03b13eb1abd92256ad0666519a79b74
MartinMenev <martin.menew@gmail.com>
2022-09-12 11:32:38 +0300

    New code

---
+++ src/JAVA_ADVANCED/Stacks_Queues/MaximumElement.java
@@ -0,0 +1,29 @@
+package JAVA_ADVANCED.Stacks_Queues;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static java.util.Collections.max;
+
+public class MaximumElement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = Integer.parseInt(scanner.nextLine());
+        ArrayDeque <Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < num; i++) {
+          int [] tokens = Arrays
+                  .stream(scanner.nextLine().split(" "))
+                  .mapToInt(Integer::parseInt)
+                  .toArray();
+
+
+            switch (tokens[0]) {
+                case 1 -> stack.push(tokens[1]);
+                case 2 -> stack.pop();
+                case 3 -> System.out.println(max(stack));
+            }
+        }
+    }
+}
