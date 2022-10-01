ffcb7f1b1c3a230a80a3075e0015fb6bc8a9fe8e
MartinMenev <martin.menew@gmail.com>
2022-10-01 14:32:17 +0300

    New code

---
+++ src/JAVA_ADVANCED/Defining_Classes/RawData/Tire.java
@@ -0,0 +1,16 @@
+package JAVA_ADVANCED.Defining_Classes.RawData;
+
+public class Tire {
+    private final double pressure;
+    private final int age;
+
+    public Tire(double pressure, int age) {
+        this.pressure = pressure;
+        this.age = age;
+    }
+
+    public double getPressure() {
+        return pressure;
+    }
+}
+
