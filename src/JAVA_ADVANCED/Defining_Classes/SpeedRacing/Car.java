98dd04419e3987e9658a99e6b544287f18305963
MartinMenev <martin.menew@gmail.com>
2022-10-01 12:58:26 +0300

    New code

---
+++ src/JAVA_ADVANCED/Defining_Classes/SpeedRacing/Car.java
@@ -0,0 +1,28 @@
+package JAVA_ADVANCED.Defining_Classes.SpeedRacing;
+
+public class Car {
+    private final String model;
+    private double fuelAmount;
+    private final double fuelCostPerKm;
+    private int distanceTravelled = 0;
+
+    public Car(String model, int fuelAmount, double fuelCostPerKm) {
+        this.model = model;
+        this.fuelAmount = fuelAmount;
+        this.fuelCostPerKm = fuelCostPerKm;
+    }
+
+    public boolean canMove (int distance) {
+
+       if(fuelAmount < fuelCostPerKm * distance) return false;
+
+       fuelAmount -= fuelCostPerKm * distance;
+        distanceTravelled += distance;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %.2f %d", model, fuelAmount, distanceTravelled);
+    }
+}
