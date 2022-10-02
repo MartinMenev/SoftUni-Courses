e7d6308a30b9ad6bbfb1dc2b07856f0e63f365c1
MartinMenev <martin.menew@gmail.com>
2022-10-02 16:37:51 +0300

    New code

---
+++ src/JAVA_ADVANCED/Defining_Classes/Google/Parent.java
@@ -0,0 +1,36 @@
+package JAVA_ADVANCED.Defining_Classes.Google;
+
+public class Parent {
+    private String parentName;
+    private String parentBirthday;
+
+    public Parent(String parentName, String parentBirthday) {
+        this.parentName = parentName;
+        this.parentBirthday = parentBirthday;
+    }
+
+    public String getParentName() {
+        return parentName;
+    }
+
+    public void setParentName(String parentName) {
+        this.parentName = parentName;
+    }
+
+    public String getParentBirthday() {
+        return parentBirthday;
+    }
+
+    public void setParentBirthday(String parentBirthday) {
+        this.parentBirthday = parentBirthday;
+    }
+
+    @Override
+    public String toString() {
+        String result = "Parents:%n";
+        if (!parentName.isBlank())
+            result = parentName + " " + parentBirthday;
+
+        return result;
+    }
+}
