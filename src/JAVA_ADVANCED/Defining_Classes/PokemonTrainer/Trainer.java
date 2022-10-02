c34220685f7ba4d20ec57b6a6579aeab55e3f75f
MartinMenev <martin.menew@gmail.com>
2022-10-02 12:30:02 +0300

    New code

---
+++ src/JAVA_ADVANCED/Defining_Classes/PokemonTrainer/Trainer.java
@@ -0,0 +1,71 @@
+package JAVA_ADVANCED.Defining_Classes.PokemonTrainer;
+
+import java.util.List;
+
+public class Trainer {
+    private String name;
+    private int numberBadges = 0;
+    private List<Pokemon> pokemonList;
+
+    public Trainer(String name, List<Pokemon> pokemonList) {
+        this.name = name;
+        this.pokemonList = pokemonList;
+    }
+
+    public void hasOnePokemon (String element) {
+
+        if (!this.getPokemonList().isEmpty()) {
+            boolean isFound = false;
+
+            for (Pokemon pokemon : getPokemonList()) {
+                if (pokemon.getElement().equals(element)) {
+                    numberBadges++;
+                    isFound = true;
+                    break;
+                }
+            }
+            if (!isFound) {
+                for (Pokemon pokemon : getPokemonList()) {
+                    pokemon.setHealth(pokemon.getHealth() - 10);
+                    if (pokemon.getHealth() <= 0) {
+                        pokemonList.remove(pokemon);
+                        if (getPokemonList().isEmpty())
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (!pokemonList.isEmpty()) {
+            return  name + " " + numberBadges + " " + getPokemonList().size();
+        }
+        return name + " " + numberBadges + " 0";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberBadges() {
+        return numberBadges;
+    }
+
+    public void setNumberBadges(int numberBadges) {
+        this.numberBadges = numberBadges;
+    }
+
+    public List<Pokemon> getPokemonList() {
+        return pokemonList;
+    }
+
+    public void setPokemonList(List<Pokemon> pokemonList) {
+        this.pokemonList = pokemonList;
+    }
+}
