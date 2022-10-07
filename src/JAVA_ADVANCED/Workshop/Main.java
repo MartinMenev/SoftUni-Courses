25e48b70a7e736b9d5a2f605e2247784f9ca04d7
MartinMenev <martin.menew@gmail.com>
2022-10-07 15:30:27 +0300

    New code

---
+++ src/JAVA_ADVANCED/Workshop/Main.java
@@ -0,0 +1,20 @@
+package JAVA_ADVANCED.Workshop;
+
+public class Main {
+    public static void main(String[] args) {
+
+        LinkedList list = new LinkedList();
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(3);
+        list.addLast(4);
+
+        System.out.println(list.get(0));
+        System.out.println(list.removeFirst());
+        System.out.println(list.removeFirst());
+
+        System.out.println();
+
+
+    }
+}
