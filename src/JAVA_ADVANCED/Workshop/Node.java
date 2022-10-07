099fc2439c29a695a51acab5422d594c2390cb4b
MartinMenev <martin.menew@gmail.com>
2022-10-07 15:30:55 +0300

    New code

---
+++ src/JAVA_ADVANCED/Workshop/Node.java
@@ -0,0 +1,17 @@
+package JAVA_ADVANCED.Workshop;
+
+public class Node {
+
+    public int value;
+    public Node next;
+    public Node prev;
+
+    public Node(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value + "";
+    }
+}
