f13d70afdb3386b0e887059562bae382ec5d0ac1
MartinMenev <martin.menew@gmail.com>
2022-10-08 14:51:18 +0300

    New code

--- src/JAVA_ADVANCED/Generics/GenericBox/Box.java
+++ src/JAVA_ADVANCED/Generics/GenericBox/Box.java
@@ -1,14 +1,21 @@
 package JAVA_ADVANCED.Generics.GenericBox;
 
-public class Box <T> {
+
+public class Box <T extends Comparable<T>> {
     public T value;
 
     public Box(T value) {
         this.value = value;
     }
 
+
     @Override
     public String toString() {
         return String.format("%s: %s", value.getClass().getTypeName(),value);
     }
-}
+
+    public int compareTo(Box<T> element) {
+        return this.value.compareTo(element.value);
+        }
+    }
+
