c2da4987d6d5a4e480764953d7e56cf25850b383
MartinMenev <martin.menew@gmail.com>
2022-10-08 14:51:29 +0300

    New code

--- src/JAVA_ADVANCED/Generics/GenericBox/Main.java
+++ src/JAVA_ADVANCED/Generics/GenericBox/Main.java
@@ -1,15 +1,24 @@
 package JAVA_ADVANCED.Generics.GenericBox;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        List<Box<Double>> boxes = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        int n = Integer.parseInt(scanner.nextLine());
-
-        while (n-- > 0) {
-            Box<java.lang.String> box = new Box<>(scanner.nextLine());
-            System.out.println(box.toString());
+        int linesCount = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < linesCount; i++) {
+            boxes.add(new Box<>(Double.parseDouble(scanner.nextLine())));
         }
+        Box<Double> elementBox = new Box<>(Double.parseDouble(scanner.nextLine()));
+        System.out.println(countGreater(boxes, elementBox));
+    }
+
+    private static <T extends Comparable<T>> long countGreater(List<Box<T>> boxes, Box<T> elementBox) {
+        return boxes.stream()
+                .filter(b -> b.compareTo(elementBox) > 0)
+                .count();
     }
 }
