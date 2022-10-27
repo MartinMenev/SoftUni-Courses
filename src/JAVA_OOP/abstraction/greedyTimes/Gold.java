ef90dba1f2221ee12ed6df3fbd63e418ad264357
MartinMenev <martin.menew@gmail.com>
2022-10-27 07:31:33 +0300

    New code

---
+++ src/JAVA_OOP/abstraction/greedyTimes/Gold.java
@@ -0,0 +1,18 @@
+package JAVA_OOP.abstraction.greedyTimes;
+
+public class Gold {
+    private int amount;
+
+    public Gold(int amount) {
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+}
