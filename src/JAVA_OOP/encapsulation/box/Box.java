f502e48f74fca2fda2be0112d0a442f7e814d72f
MartinMenev <martin.menew@gmail.com>
2022-10-28 06:29:33 +0300

    New code

---
+++ src/JAVA_OOP/encapsulation/box/Box.java
@@ -0,0 +1,53 @@
+package JAVA_OOP.encapsulation.box;
+
+public class Box {
+    private double length;
+    private double width;
+    private double height;
+
+    public Box(double length, double width, double height) {
+        this.setLength(length);
+        this.setHeight(height);
+        this.setWidth(width);
+
+    }
+
+    private void setLength(double length) {
+        if (length > 0) {
+            this.length = length;
+        } else throw new IllegalArgumentException("Length cannot be zero or negative.");
+    }
+
+    private void setWidth(double width) {
+        if (width > 0) {
+            this.width = width;
+        } else throw new IllegalArgumentException("Width cannot be zero or negative.");
+    }
+
+    private void setHeight(double height) {
+        if (height > 0) {
+            this.height = height;
+        } else throw new IllegalArgumentException("Height cannot be zero or negative.");
+    }
+
+//    private boolean negativeValue(String name, double value) {
+//        if (value <= 0) {
+//            System.out.println(name + " cannot be zero or negative.");
+//            negativeEntry = true;
+//            return true;
+//        }
+//        return false;
+//    }
+
+    public double calculateSurfaceArea() {
+            return 2 * length * width + 2 * length * height + 2 * width * height;
+    }
+
+    public double calculateLateralSurfaceArea() {
+            return 2 * length * height + 2 * width * height;
+    }
+
+    public double calculateVolume() {
+            return length * width * height;
+    }
+}
