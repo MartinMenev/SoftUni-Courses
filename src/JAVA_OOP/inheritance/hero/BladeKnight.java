87f94d2eaa163379b12a0131c44bbab292bfcfaf
MartinMenev <martin.menew@gmail.com>
2022-10-30 13:05:52 +0200

    New code

---
+++ src/JAVA_OOP/inheritance/hero/BladeKnight.java
@@ -0,0 +1,9 @@
+package JAVA_OOP.inheritance.hero;
+
+public class BladeKnight extends DarkKnight {
+    public BladeKnight(String username, int level) {
+        super(username, level);
+    }
+
+
+}
---
+++ src/JAVA_OOP/inheritance/hero/Main.java
@@ -0,0 +1,14 @@
+package JAVA_OOP.inheritance.hero;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        SoulMaster soulMaster = new SoulMaster("Martin", 3);
+
+        System.out.println(soulMaster);
+
+    }
+}
