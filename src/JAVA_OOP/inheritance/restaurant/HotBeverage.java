2bf3b67e60d4cd8bff82e627516861e56740a5ef
MartinMenev <martin.menew@gmail.com>
2022-11-01 07:13:36 +0200

    New code

---
+++ src/JAVA_OOP/inheritance/restaurant/HotBeverage.java
@@ -0,0 +1,12 @@
+package JAVA_OOP.inheritance.restaurant;
+
+import java.math.BigDecimal;
+
+public class HotBeverage extends Beverage {
+
+
+    public HotBeverage(String name, BigDecimal price, double milliliters) {
+        super(name, price, milliliters);
+    }
+
+}
