0e560dea422b1de2628f509e954628e79e15d41c
MartinMenev <martin.menew@gmail.com>
2022-11-02 06:21:28 +0200

    New code

---
+++ src/JAVA_OOP/InterfacesAndAbstraction/personInterface/Citizen.java
@@ -0,0 +1,21 @@
+package JAVA_OOP.InterfacesAndAbstraction.personInterface;
+
+public class Citizen implements Person{
+   private String name;
+   private int age;
+
+    public Citizen(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int getAge() {
+        return this.age;
+    }
+}
