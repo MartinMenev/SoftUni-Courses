117ede124c2b1273191833ffdc46bab93aa25894
MartinMenev <martin.menew@gmail.com>
2022-11-03 07:06:05 +0200

    New code

---
+++ src/JAVA_OOP/interfacesAndAbstraction/foodShortage/Birthable.java
@@ -0,0 +1,6 @@
+package JAVA_OOP.interfacesAndAbstraction.foodShortage;
+
+public interface Birthable {
+
+    String getBirthDate();
+}
---
+++ src/JAVA_OOP/interfacesAndAbstraction/foodShortage/Buyer.java
@@ -0,0 +1,6 @@
+package JAVA_OOP.interfacesAndAbstraction.foodShortage;
+
+public interface Buyer {
+    void buyFood();
+    int getFood();
+}
